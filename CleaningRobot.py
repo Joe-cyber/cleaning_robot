0eb79ea96e96bac0d5996e4a4289864df949e571
Joe-cyber <caliendo.gioacchino00@gmail.com>
2022-12-13 17:08:50 +0100

    Revert "test implement execute command not valid red"
    This reverts commit 41c9d14e152258135de6b6cd0a461ab83c792461.

--- CleaningRobot.py
+++ CleaningRobot.py
@@ -109,7 +109,6 @@ class CleaningRobot:
             self.activate_rotation_motor(self.RIGHT)
             ind = dir.index(self.facing)
             self.facing = dir.pop(3) if ind == 0 else dir.pop(ind - 1)
-
         return self.robot_status()
 
     def obstacle_found(self) -> bool:
