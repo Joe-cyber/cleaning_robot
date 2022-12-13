8be0bb6886d5a463ae5d76e9cd253692ee560184
Joe-cyber <caliendo.gioacchino00@gmail.com>
2022-12-13 16:27:34 +0100

    test battery is under 10 percent red

--- test/CleaningRobotTest.py
+++ test/CleaningRobotTest.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 from CleaningRobot import CleaningRobot
 from CleaningRobotError import CleaningRobotError
+from mock import GPIO
 
 
 class CleaningRobotTest(unittest.TestCase):
@@ -13,3 +14,9 @@ class CleaningRobotTest(unittest.TestCase):
         robot.initialize_robot()
         self.assertEqual("(0,0,N)", robot.robot_status())
 
+    @patch.object(GPIO, 'input')
+    def test_battery_is_under_10_percent(self, mock_input):
+        mock_input.return_value = 5
+        robot = CleaningRobot(3, 3)
+        robot.initialize_robot()
+        self.assertTrue(robot.battery_led_on)
